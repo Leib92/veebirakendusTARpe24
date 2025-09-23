c5349d4be5a227763da30e7722770e6772dfd0de
Leib92 <141932460+Leib92@users.noreply.github.com>
2025-09-23 15:25:50 +0300

    Update the entire project
    - Copied pasted files from zone.ee
    - Added vormid

---
+++ harjutused/Vormide Loomine (UL3)/lugemineScript.js
@@ -0,0 +1,8 @@
+function tervitus() {
+    let vastus = document.getElementById("vastus");
+    let nimi = document.getElementById("nimi");
+
+    vastus.innerHTML = "Tere tulemast ja vasta küsimustele "+
+    nimi.value + "!" + " <br>Sa õpid juba " + aastat.value+ " aastat!";
+    vastus.style.color = "red";
+}
