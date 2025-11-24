09e56ef6f69dbe37ee45752b919009bb073e99d1
Leib92 <allan.leibenau92@gmail.com>
2025-11-24 12:36:53 +0200

    Tegeleb dropdown osaga nav.php

--- harjutused/PHP/phpindex/nav.php
+++ harjutused/PHP/phpindex/nav.php
@@ -3,23 +3,33 @@
         <li>
             <a class="active" href="?leht=kodu.php">Kodu</a>
         </li>
-        <li>
-            <a href="?leht=jsToo.php">JS Joonis</a>
+        <li class="dropdown">
+            <a class="dropbtn">Javascript</a>
+            <ul class="dropdown-content">
+                <li>
+                    <a href="?leht=jsToo.php">JS Joonis</a>
+                </li>
+                <li>
+                    <a href="?leht=kalkulaator.php">JS Veebikalkulaator</a>
+                </li>
+            </ul>
+        </>
+        <li class="dropdown">
+            <a class="dropbtn">Funktsioonid</a>
+            <ul class="dropdown-content">
+                <li>
+                    <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
+                </li>
+                <li>
+                    <a href="?leht=tekstifunktsioonid.php">Tekstifunktsioonid</a>
+                </li>
+                <li>
+                    <a href="?leht=matemaatilised.php">Matemaatika Funktsioonid</a>
+                </li>
+            </ul>
         </li>
         <li>
-            <a href="?leht=kalkulaator.php">JS Veebikalkulaator</a>
-        </li>
-        <li>
-            <a href="?leht=ajafunktsioonid.php">Ajafunktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=tekstifunktsioonid.php">Tekstifunktsioonid</a>
-        </li>
-        <li>
-            <a href="?leht=matemaatilised.php">Matemaatika Funktsioonid</a>
-        </li>
-        <li>
-            <a href="allanleibenau23.thkit.ee" target="_blank">Vana index</a>
+            <a href="?allanleibenau23.thkit.ee" target="_blank">Vana index</a>
         </li>
         <li>
             <a href="?leht=gitKasud.php">GIT Käsud</a>
